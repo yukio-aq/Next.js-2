7d7bce7e2725c39600a8f8c42decd1ac5c086764
kazuyuki-hosoda <kazuyuki.hosoda@asia-quest.jp>
2022-11-09 16:14:01 +0900

    fallbackの挙動

--- src/pages/SG/[id].js
+++ src/pages/SG/[id].js
@@ -1,14 +1,22 @@
+import { useRouter } from "next/router"
+
 export default function Page({ id }) {
+    const router = useRouter();
+    if(router.isFallback) {
+        return <h3>Loading</h3>
+    }
+
     return <h3>{id}</h3>
 }
 
 export async function getStaticPaths() {
     return {
         paths: [{ params: { id: "1" }},{ params: { id: "2" }}],
-        fallback: false,
+        fallback: true, //falseの場合、設定したID以外は404となる。
     }
 }
 
+//routerを設定することで存在しないID(3〜)がリクエストされると、Node.jsが代わりに処理する。
 export async function getStaticProps({ params }) {
     return {
         props: {
